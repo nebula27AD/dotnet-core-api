9e915eca5d18cdb0b1ce7b08515f976c85ba4102
nebula27AD <64992193+nebula27AD@users.noreply.github.com>
2023-11-27 09:47:38 -0800

    Update Startup.cs

--- Startup.cs
+++ Startup.cs
@@ -59,7 +59,7 @@ namespace TodoApi
             app.UseSwagger();
 
             // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.),
-            // specifying the Swagger JSON endpoint.
+            /// specifying the Swagger JSON endpoint.
             app.UseSwaggerUI(c =>
             {
                 c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
